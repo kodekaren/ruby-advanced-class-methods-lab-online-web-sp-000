ab5949c079542e0f4394a0173cf60379962e03a7
karen ercoli <karenercoli@gmail.com>
2018-12-02 22:22:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+def self.create
+  song = self.new
+  song.save
+  song
+end
+
   def self.all
     @@all
   end
@@ -10,4 +16,28 @@ class Song
     self.class.all << self
   end
 
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
 end
+
+def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  song.save
+  song
+end
+
+def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name == nil
+      self.create_by_name
+    end
+  end
+
+end
+ 
